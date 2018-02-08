0fe7e1a5a9568b1ae61c14f363d098b6853d82de
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-02-08 12:31:05 -0500

    Fixing some indentation issues in the gatorgrader script.

--- gatorgrader.sh
+++ gatorgrader.sh
@@ -169,10 +169,10 @@ if [ "$CHECK" = true ]; then
   python3 gatorgrader/gatorgrader.py --nowelcome --directories src/www \
                                      --checkfiles travels.html --fragments "<time>" --fragmentcounts 2
   determine_exit_code $?
-# --> GatorGrader CHECK: the reflection contains at least 4 paragraphs with 3 sentences each
+  # --> GatorGrader CHECK: the reflection contains at least 4 paragraphs with 3 sentences each
   python3 gatorgrader/gatorgrader.py --nowelcome --directories writing --checkfiles reflection.md --paragraphs 1 --sentences 3
   determine_exit_code $?
-    # --> GatorGrader CHECK: the repository contains at least five commits (update to current + 5)
+  # --> GatorGrader CHECK: the repository contains at least five commits (update to current + 5)
   python3 gatorgrader/gatorgrader.py --nowelcome --commits 5
   determine_exit_code $?
   echo ""
